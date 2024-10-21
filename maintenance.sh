cee5ab5e0fe86a1c0dea4afa4befae71efeb2210
mkoterski <matthiaskoterski@gmail.com>
2024-10-21 21:30:23 +0200

    koteshi's linux maintenance script v0.1
    running some simple automation tasks

--- maintenance.sh
+++ maintenance.sh
@@ -1,1 +1,27 @@
-# maintenance.sh
+#!/bin/bash
+
+# 
+# koteshi's linux maintenance script v0.1
+# 
+
+
+# Update and upgrade the system
+sudo apt update
+sudo apt upgrade -y
+sudo apt autoremove -y
+
+# Check if a reboot is needed and schedule it
+if [ -f /var/run/reboot-required ]; then
+    sudo shutdown -r +5
+fi
+
+# Check if Pi-hole is installed and update it
+if command -v pihole &> /dev/null; then
+    pihole -up
+fi
+
+# Self-update script
+if [ -f /usr/local/bin/maintenance.sh ]; then
+    curl -s https://raw.githubusercontent.com/mkoterski/lnx-toolbox/main/maintenance.sh -o /usr/local/bin/maintenance.sh
+    chmod +x /usr/local/bin/maintenance.sh
+fi
